e15063c6a535cfa65a730e8fd49bffebcc8e0535
Akash gupta <akash.teoota@gmail.com>
2024-09-29 13:12:12 +0530

    Updated

---
+++ lib/appConstants.dart
@@ -0,0 +1,13 @@
+class AppConstants {
+  static const String app_name = 'Emallbazar';
+  static const String androidPackageName = 'eShop.multivendor.customer';
+  static const String iosPackageName = 'wrteam.eshop.multivendor';
+  static const String baseUrl = 'https://emallbazar.com/app/v1/api/';
+  static const String chatBaseUrl = 'https://emallbazar.com/app/v1/Chat_Api/';
+  // static const String baseUrl =
+  //     'https://vendoreshop.wrteam.co.in/app/v1/api/'; //dev
+  // static const String chatBaseUrl =
+  //     'https://vendoreshop.wrteam.co.in/app/v1/Chat_Api/'; //dev
+  static const String shareNavigationWebUrl = 'vendor.eshopweb.store';
+  static const String defaultLanguage = 'en';
+}
